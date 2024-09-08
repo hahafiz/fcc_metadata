f001543fecaeb8f996249ca041b68904402b2a0a
Qamarul Hafiz <hafizqamarul19@gmail.com>
2024-09-08 13:35:41 +0000

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: './public/data/uploads/' })
 
 var app = express();
 
@@ -9,6 +11,16 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+   // req.file is the name of your file in the form above, here 'uploaded_file'
+   // req.body will hold the text fields, if there were any 
+   res.json({
+     name: req.file.originalname,
+     type: req.file.mimetype,
+     size: req.file.size
+   });
 });
 
 
